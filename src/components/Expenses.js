60d125084b859eac62bf81c066c5ec0beb510e8b
kshitizbali <kbali@presto.com>
2022-06-27 17:26:39 -0700

    fixing list props data error

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -5,24 +5,24 @@ function Expenses(props) {
   return (
     <div className="expenses">
       <ExpenseItem
-        title={props[0].title}
-        amount={props[0].amount}
-        date={props[0].date}
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={props[1].title}
-        amount={props[1].amount}
-        date={props[1].date}
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={props[2].title}
-        amount={props[2].amount}
-        date={props[2].date}
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={props[3].title}
-        amount={props[3].amount}
-        date={props[3].date}
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
       ></ExpenseItem>
     </div>
   );
